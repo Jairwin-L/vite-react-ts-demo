83f14b4ed96c9e0d29cde321c4919e9db746a672
Jairwin <jairwin@jairwin.cn>
2021-06-03 23:41:19 +0800

    fix: 问题复现

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
-import { Button } from 'antd-mobile';
+import React, { useEffect, useState } from 'react'
+import { useHistory } from "react-router-dom"
+import { List, InputItem, Button } from 'antd-mobile';
+import '@css/login.less'
 
-export default () => {
+export default (): JSX.Element => {
+	const [formValues, setFormValues] = useState({
+		name: '',
+	});
+	useEffect(() => {
+		// document.title = `登录 | ${window.title}`
+	}, [])
+	const onLogin = () => {
+		console.log('form=====>：', formValues);
+	}
+
+	const updateFormValues = (values: any) => {
+    setFormValues((state) => {
+      return {
+        ...state,
+        ...values,
+      };
+    });
+  };
+
 	return (
-		<>
-			<Button type="primary">按钮</Button>
-		</>
-	);
+		<div className="login_container animated fadeIn">
+			<div className="login_main">
+				<div className="login_form">
+					<h3>登录</h3>
+					{/* TODO:报错如图：vendor.dc84d456.js:27 ReferenceError: require is not defined */}
+					<List>
+						<InputItem
+							clear
+							value={formValues.name}
+							placeholder="请输入名字"
+							onChange={(value) => updateFormValues({ name: value })}
+							className="input"
+						>
+							<span className="info">名字</span>
+						</InputItem>
+					</List>
+					<Button className="login_btn" type="primary" onClick={onLogin}>登录</Button>
+				</div>
+			</div>
+		</div>
+	)
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,76 +1,62 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import VitePluginCdn from 'vite-plugin-cdn'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 import legacyPlugin from '@vitejs/plugin-legacy';
 const path = require('path');
 const resolvePath = dir => path.join(__dirname, dir);
 
 export default defineConfig({
-	plugins: [reactRefresh(),
-	legacyPlugin({
-		targets: [
-			'Android > 39',
-			'Chrome >= 60',
-			'Safari >= 10.1',
-			'iOS >= 10.3',
-			'Firefox >= 54',
-			'Edge >= 15',
-		],
-	}),
-	VitePluginCdn({
-		esm: true,
-		modules: [
-			{
-				name: 'antd-mobile',
-				css: 'https://lib.baomitu.com/antd-mobile/2.3.4/antd-mobile.min.css',
-				url: 'https://lib.baomitu.com/antd-mobile/2.3.4/antd-mobile.min.js'
-			}
-		]
-	})
-		// styleImport({
-		// 	libs: [
-		// 		{
-		// 			libraryName: 'antd',
-		// 			esModule: true,
-		// 			resolveStyle: (name) => {
-		// 				return `antd/es/${name}/style/index`;
-		// 			},
-		// 		},
-		// 	],
-		// }),
+	plugins: [
+		reactRefresh(),
+		legacyPlugin({
+			targets: [
+				'Android > 39',
+				'Chrome >= 60',
+				'Safari >= 10.1',
+				'iOS >= 10.3',
+				'Firefox >= 54',
+				'Edge >= 15',
+			],
+		}),
 	],
-	build: {
-		// target: 'esnext',
-		// minify: 'terser',
-		// manifest: false,
-		// sourcemap: false,
-		// outDir: 'build',
-		rollupOptions: {
-			external: ['antd-mobile'],
-		}
-	},
-	esbuild: {},
-	optimizeDeps: {},
 	server: {
-		port: 9090,
+		host: 'dev.jairwin.cn',
+		port: 7083,
 	},
 	css: {
 		preprocessorOptions: {
 			less: {
+				modifyVars: {
+					'@primary_btn': '#2598ff',
+					'@detail_btn': '#2db7f5',
+					'@edit_btn': '#8354ee',
+					'@delete_btn': '#ff4d4f',
+					'@white': '#fff',
+					'@black': '#333',
+					'@pink': '#ffc0cb',
+					'@divideColor': '#f1f5ff',
+					'@textColor': '#999',
+					'@themeColor': '#3e71f8',
+					'@gray': '#7a7a7a',
+					'@defaultFontSize': '16px',
+					'@commonFontSize': '14px',
+					'@smallFontSize': '12px',
+				},
 				javascriptEnabled: true,
 			},
 		},
 	},
-	// resolve: {
-	// 	alias: [
-	// 		{
-	// 			find: /^~/,
-	// 			replacement: path.resolve(__dirname, "src"),
-	// 		},
-	// 	],
-	// },
 	resolve: {
 		// 自动解析确定的扩展，可以在引入模块时不带扩展
 		extensions: ['.js', '.jsx', '.json', '.less', '.ts', '.tsx'],
+		// 路径别名
+		alias: {
+			'@': resolvePath('src'),
+			'@constant': resolvePath('src/constant'),
+			'@components': resolvePath('src/components'),
+			'@api': resolvePath('src/api'),
+			'@utils': resolvePath('src/utils'),
+			'@css': resolvePath('src/assets/css'),
+			'@img': resolvePath('src/assets/img'),
+		}
 	},
 })
